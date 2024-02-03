84ec69a3188c11f7165f66051a2048cb408d2dae
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-02-03 20:34:20 +0600

    added form and click box

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const button = document.querySelector('#button');
+// button.style.cssText
 
-// button.addEventListener('click', function(){
-//     console.log('clickable');
-// })
-
-button.addEventListener('mouseover', function(){
-    console.log('Mouse over');
+button.addEventListener('click', function(){
+    const box = document.querySelector('.box');
+    box.style.color = 'White';
+    box.style.backgroundColor = 'blue';
+    // console.log();
 })
+
+
+
+
+const showButton = document.querySelector('#sub_button');
+  
+showButton.addEventListener('click', function(){
+    const inputName = document.querySelector('#input_name');
+    const name = inputName.value;
+    // console.log(name);
+
+    const inputEmail = document.querySelector('#input_email');
+    const email = inputEmail.value;
+
+    const combined = name + "<br>" + email;
+
+    // console.log(email);
+
+    const message = document.querySelector('.show');
+
+    const div = document.createElement('div');
+    div.innerHTML = combined;
+
+    message.appendChild(div);
+})
+
